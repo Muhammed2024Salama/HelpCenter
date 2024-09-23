c47e21baf43932fa95fc694f3652049828f9de78
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-09-23 16:00:46 +0300

    Refactor Seending Mail

--- app/Repository/ContactSupportRepository.php
+++ app/Repository/ContactSupportRepository.php
@@ -17,8 +17,15 @@ class ContactSupportRepository implements ContactSupportInterface
 
     public function store(array $data)
     {
+        // Send the email
         Mail::to($data['email'])->send(new SimpleMail($data['email'], $data['question']));
 
-        return 'Email has been sent successfully!';
+        // Save data to the database after sending the email
+        $contact = ContactSupport::create([
+            'email' => $data['email'],
+            'question' => $data['question']
+        ]);
+
+        return 'Email has been sent successfully and contact support record has been created!';
     }
 }
